74954d8b78cfa03c6da73c67d4566116830355e5
김광준 <kwangjun3952@gmail.com>
2019-10-24 18:42:40 +0900

    이미지 뷰 올리고 터치 제스쳐 달기, info.plist의 앨범 허용 메시지 작성

--- PJT2-SignUp/PJT2-SignUp/SecondViewController.swift
+++ PJT2-SignUp/PJT2-SignUp/SecondViewController.swift
@@ -34,6 +34,43 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     private func addSecondVCImgView() {
         
+        secondVCImgView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let guide = view.safeAreaLayoutGuide
+        
+        self.view.addSubview(secondVCImgView)
+        
+        secondVCImgView.backgroundColor = .lightGray
+        
+        secondVCImgView.contentMode = .scaleAspectFit
+        
+        let secondImgViewTop: NSLayoutConstraint
+        secondImgViewTop = secondVCImgView.topAnchor.constraint(equalTo: guide.topAnchor, constant: 20)
+        
+        let secondImgViewLeading: NSLayoutConstraint
+        secondImgViewLeading = secondVCImgView.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 20)
+        
+        let secondImgViewWidth: NSLayoutConstraint
+        secondImgViewWidth = secondVCImgView.widthAnchor.constraint(equalToConstant: 200)
+        
+        let secondImgViewHeight: NSLayoutConstraint
+        secondImgViewHeight = secondVCImgView.heightAnchor.constraint(equalToConstant: 200)
+        
+        secondImgViewTop.isActive = true
+        secondImgViewLeading.isActive = true
+        secondImgViewWidth.isActive = true
+        secondImgViewHeight.isActive = true
+        
+        /// ImageView에 Tap Gesture 달아주기
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTappedImgView(_:)))
+        
+        secondVCImgView.isUserInteractionEnabled = true
+        secondVCImgView.addGestureRecognizer(tapGestureRecognizer)
+        
+    }
+    
+    @objc func didTappedImgView(_ sender: UIImageView) {
+        self.present(self.imgPicker, animated: true, completion: nil)
     }
 
 }
